d3b45f01c9c7edf08230298000ecb9f89125cad5
Bhavyah Ramesh <ramgayubhavyah@gmail.com>
2019-08-31 22:17:45 +0530

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('portfolio.html')
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+if __name__ == '__main__':
+    app.run(debug=True)
